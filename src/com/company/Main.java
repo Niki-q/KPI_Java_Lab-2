e4d573cb9d917957b9522b3ef674ea222a343311
Nikita <petrychenko.nikita.learn@gmail.com>
2021-10-27 13:17:03 +0300

    4th step

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -15,13 +15,27 @@ public class Main {
         while (Objects.equals(main_choise, "y"));
     }
     static int add(String input){
-        final String[] delimiters = new String[] {"\\n",","};
-        final List<String> delimiter_list = Arrays.asList(delimiters);
+        final String[] delimiters = new String[] {"\\n"};
+
+        ArrayList<String> delimiter_list = new ArrayList<>(Arrays.asList(delimiters));
+
+        if (isDelimiterParams(input)){
+            String delimiters_params = input.substring(2,input.indexOf("\\n"));
+            String[] delimiters_arr = new String[] {delimiters_params};
+            delimiter_list.addAll(Arrays.asList(delimiters_arr));
+            input = input.replace(input.substring(0,input.indexOf("\\n")+2),"");
+        }
+        else
+            delimiter_list.add(",");
+
         input = Utility.delimiterConverter(input, delimiter_list,"`");
         out.println(input);
         List<String> number_elements = Arrays.asList(input.split("`"));
         out.println(number_elements);
         int sum = Utility.sumOfListItem(number_elements);
         return sum;
+    }
+    static boolean isDelimiterParams(String str){
+        return str.contains("//");
     }
 }
